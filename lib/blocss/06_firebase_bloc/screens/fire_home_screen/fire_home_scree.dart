69175e8b762119e1328641489c7b4a86712117bb
UbAidu21 <ubaid5523911@gmail.com>
2024-01-10 20:58:08 +0500

    video and firebse

---
+++ lib/blocss/06_firebase_bloc/screens/fire_home_screen/fire_home_scree.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FireHomeScreen extends StatefulWidget {
+  const FireHomeScreen({super.key});
+
+  @override
+  State<FireHomeScreen> createState() => _FireHomeScreenState();
+}
+
+class _FireHomeScreenState extends State<FireHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
-import 'package:bloc_learning/blocss/05_multi_bloc/screens/home_screen.dart';
+import 'package:bloc_learning/blocss/06_firebase_bloc/screens/fire_home_screen/fire_home_scree.dart';
+import 'package:bloc_learning/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -18,7 +22,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         textTheme: const TextTheme(bodyMedium: TextStyle(fontSize: 20)),
       ),
-      home: const HomePage(),
+      home: const FireHomeScreen(),
     );
   }
 }
